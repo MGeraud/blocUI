e9a23d808f480a1da5fb292036d22b5392ac0abd
Geraud <52377295+MGeraud@users.noreply.github.com>
2021-05-20 15:22:59 +0200

    create logo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LogoECA from "./components/logo/LogoECA";
+
+const App = () => {
+  return (
+    <div className="App">
+      <LogoECA />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/logo/LogoECA.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Logo.css";
+
+const LogoECA = () => {
+  return (
+    <div className="logo-body">
+      <div>
+        <h1>ECA</h1>
+      </div>
+      <div>
+        <p>Escapade Club Albi</p>
+      </div>
+    </div>
+  );
+};
+
+export default LogoECA;
